37d1c4da235b71debca821477be3526a22d5e248
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-06-29 22:01:40 +0300

    added login, footer, nav components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './features/home/home.component';
+import { LoginComponent } from './features/login/login.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
