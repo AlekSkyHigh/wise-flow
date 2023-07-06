6beb96f9864c5102c3e59ad8fccba41b5ca0a22f
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-07-06 22:13:07 +0300

    new mongoose model added

---
+++ server/models/Entry.js
@@ -0,0 +1,31 @@
+const { Schema, model, Types: { ObjectId } } = require('mongoose');
+
+const entrySchema = new Schema({
+    type: {
+        type: String,
+        enum: ['income', 'expense'],
+        required: true
+    },
+    frequency: {
+        type: String,
+        enum: ['one-time', 'regular'],
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    _ownerId: { type: ObjectId, ref: 'User', required: true }
+});
+
+const Entry = model('Entry', entrySchema);
+
+module.exports = Entry;
