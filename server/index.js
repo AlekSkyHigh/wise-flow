d7e88e5d0bee4c048478224dffdfd53dcc44ddbf
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-08-08 16:15:24 +0300

    initial database deploy

--- server/index.js
+++ server/index.js
@@ -10,7 +10,8 @@ const ccController = require('./controllers/ccController');
 const pcController = require('./controllers/pcController');
 
 
-const connectionString = 'mongodb://localhost:27017/wise-flow';
+// const connectionString = 'mongodb://localhost:27017/wise-flow';
+const connectionString = 'mongodb+srv://alekskyhigh:MgVhuBeTtdk847n@cluster0.m0wvu5i.mongodb.net/?retryWrites=true&w=majority'
 
 start();
 
