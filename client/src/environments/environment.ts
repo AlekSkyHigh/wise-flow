219e12fecfd3591ed665dc26ae6268e49ab14952
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-08-08 16:36:51 +0300

    deploy related environment adjustments

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,3 +2,8 @@
 //   production: false,
 //   apiUrl: 'http://localhost:3030/'
 // };
+
+export const environment = {
+    production: false,
+    apiUrl: 'https://wise-flow-server.onrender.com/'
+};
