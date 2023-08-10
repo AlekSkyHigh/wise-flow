f5281b4bcfcb38ddd7cee45833dbc2a2e2b20389
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-08-10 22:37:12 +0300

    Enhanced 404 Page

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
     // Import other modules and dependencies here
   ],
   declarations: [
