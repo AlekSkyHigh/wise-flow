f14020a8f78b904696151de13f291b75e844e8b4
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-08-22 18:37:12 +0300

    Unit tests added for Logout functionalities

--- client/src/app/core/navigation/navigation.component.spec.ts
+++ client/src/app/core/navigation/navigation.component.spec.ts
@@ -1,21 +1,55 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NavigationComponent } from './navigation.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { SessionService } from 'src/app/services/session.service';
+import { of, throwError } from 'rxjs';
 
-// import { NavigationComponent } from './navigation.component';
+describe('NavigationComponent', () => {
+  let component: NavigationComponent;
+  let fixture: ComponentFixture<NavigationComponent>;
+  let authServiceMock: jasmine.SpyObj<AuthService>;
+  let sessionServiceMock: jasmine.SpyObj<SessionService>;
 
-// describe('NavigationComponent', () => {
-//   let component: NavigationComponent;
-//   let fixture: ComponentFixture<NavigationComponent>;
+  beforeEach(() => {
+    authServiceMock = jasmine.createSpyObj('AuthService', ['logout', 'isAuthenticated']);
+    sessionServiceMock = jasmine.createSpyObj('SessionService', ['clearSession']);
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [NavigationComponent]
-//     });
-//     fixture = TestBed.createComponent(NavigationComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    TestBed.configureTestingModule({
+      declarations: [NavigationComponent],
+      imports: [RouterTestingModule],
+      providers: [
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: SessionService, useValue: sessionServiceMock }
+      ]
+    }).compileComponents();
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    fixture = TestBed.createComponent(NavigationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create NavigationComponent', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call authService.logout and clear session when logout is called', () => {
+    const logoutResponse = 'Logout successful';
+    authServiceMock.logout.and.returnValue(of(logoutResponse));
+
+    component.logout();
+
+    expect(authServiceMock.logout).toHaveBeenCalled();
+    expect(sessionServiceMock.clearSession).toHaveBeenCalled();
+  });
+
+  it('should handle error when authService.logout throws an error', () => {
+    const errorMessage = 'Logout failed';
+    authServiceMock.logout.and.returnValue(throwError(errorMessage));
+
+    spyOn(console, 'error'); // Spy on console.error to check if it's called
+    component.logout();
+
+    expect(console.error).toHaveBeenCalledWith(errorMessage);
+  });
+});
